7bd504614eccd4d0ce7875013f60a4036760e56a
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-14 20:34:44 +0200

    Edit the news api call class and instances

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
+from dataclasses import dataclass
 
 ENV_PATH = Path(__file__).resolve().parent.parent.parent / '.env'
 
@@ -11,4 +12,20 @@ class Settings(BaseSettings):
     
     model_config = SettingsConfigDict(env_file=ENV_PATH, env_file_encoding='utf-8', extra='ignore')
 
+@dataclass
+class NewsApiCall:
+    endpoint: str
+    parameters: dict
+    max_count: int
+
+# Setting variables that should be used from outside
+
 settings = Settings() # type: ignore
+
+news_api_calls: list[NewsApiCall] = [
+    NewsApiCall('search', {'q': '"artificial intelligence" OR "PyTorch" OR "machine learning"', 'lang': 'en'}, 6),
+    NewsApiCall('top-headlines', {'category': 'general', 'lang': 'en'}, 3),
+    NewsApiCall('top-headlines', {'category': 'general', 'country': 'hu'}, 2),
+    NewsApiCall('search', {'q':'"microcontroller" OR "PCB" OR "3D printing"', 'lang': 'en'}, 2),
+    NewsApiCall('search', {'q': '"healthcare IT" OR "medical AI"', 'lang': 'en'}, 2),
+]
