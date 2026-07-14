c21298997b245866fc355e412aa485432087991c
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-15 01:12:19 +0200

    Create FastAPI server logic

--- app/main.py
+++ app/main.py
@@ -1,12 +1,33 @@
-from app.components.news_api import get_curated_news
-from app.services.serialization import save_data, load_data
-import json
+import asyncio
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import uvicorn
 
-#news = get_curated_news()
-#save_data(news, 'news_1.pkl')
+from app.components.news_api import fetch_and_cache_news, get_news
+from app.schemas.news_schema import Article
 
-news: list[dict] = load_data('saved/news_1.pkl') # type: ignore
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(fetch_and_cache_news, 'cron', hour='6,20')
+    scheduler.start()
+    
+    from app.components.news_api import _CACHE_LOCATION
+    if not _CACHE_LOCATION.exists():
+        asyncio.create_task(fetch_and_cache_news())
+    
+    yield
+    
+    scheduler.shutdown()
 
-for news_item in news:
-    print(json.dumps(news_item, indent=4, ensure_ascii=False))
-    print('\n---\n')
+app = FastAPI(lifespan=lifespan)
+
+# Expose endpoints
+
+@app.get('/api/news', response_model=list[Article])
+def read_news():
+    return get_news()
+
+if __name__ == '__main__':
+    uvicorn.run('app.main:app', host='127.0.0.1', port=8000, reload=True)
