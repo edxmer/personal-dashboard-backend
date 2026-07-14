5e27dc14fa673707f4a6fcde050c212bfc3b2de3
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-15 01:13:19 +0200

    Change type hint of path to  'str | Path'

--- app/services/serialization.py
+++ app/services/serialization.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 import pickle
 
-def save_data(data: object, path: str) -> None:
+def save_data(data: object, path: str | Path) -> None:
     with open(path, "wb") as f:
         pickle.dump(data, f)
     
-def load_data(path: str):
+def load_data(path: str | Path):
     with open(path, "rb") as f:
         loaded_object = pickle.load(f)
     return loaded_object
