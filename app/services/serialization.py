9898701f2acdfbbc032a7243138928ad6311ffd5
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-14 21:00:17 +0200

    Remove '-> object' so the type checker won't cry

--- app/services/serialization.py
+++ app/services/serialization.py
@@ -4,7 +4,7 @@ def save_data(data: object, path: str) -> None:
     with open(path, "wb") as f:
         pickle.dump(data, f)
     
-def load_data(path: str) -> object:
+def load_data(path: str):
     with open(path, "rb") as f:
         loaded_object = pickle.load(f)
     return loaded_object
