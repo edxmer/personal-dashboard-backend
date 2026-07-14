8b77f777f3700fad0bfe6e9a8a5c0b91ca3d44d5
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-14 20:33:16 +0200

    Experimentation with the news_api

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
-from app.core.config import settings
+from app.components.news_api import get_curated_news
+from app.services.serialization import save_data, load_data
+import json
 
-print('debug' if settings.debug else 'no debug', settings.dropbox_api_key, settings.gnews_api_key)
+#news = get_curated_news()
+#save_data(news, 'news_1.pkl')
+
+news: list[dict] = load_data('saved/news_1.pkl') # type: ignore
+
+for news_item in news:
+    print(json.dumps(news_item, indent=4, ensure_ascii=False))
+    print('\n---\n')
