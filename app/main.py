ce6eec94a1bc81d7563d8946e63472a698b02412
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-17 19:03:47 +0200

    Add /tasks/today API endpoint from superprod_api

--- app/main.py
+++ app/main.py
@@ -5,20 +5,32 @@ from fastapi.security import APIKeyHeader
 from contextlib import asynccontextmanager
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+import app.api.news_api as news_api
+import app.api.superprod_api as superprod_api
 from app.core.config import settings
-from app.api.news_api import fetch_and_cache_news, get_news, should_recache
+
 from app.schemas.news_schema import Article
+from app.schemas.superprod_schema import Task
 
 # * Create API lifespan *
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(fetch_and_cache_news, 'cron', hour='6,20')
+    
+    
+    scheduler.add_job(news_api.fetch_and_cache, 'cron', hour='6,20')
+    scheduler.add_job(superprod_api.fetch_and_cache, 'interval', minutes=1)
+    
+    
     scheduler.start()
     
-    if should_recache():
-        asyncio.create_task(fetch_and_cache_news())
+    
+    if news_api.should_recache():
+        asyncio.create_task(news_api.fetch_and_cache())
+    
+    if superprod_api.should_recache():
+        asyncio.create_task(superprod_api.fetch_and_cache())
     
     yield
     
@@ -46,9 +58,12 @@ app = FastAPI(lifespan=lifespan, dependencies=[Depends(validate_api_key)])
 # * API endpoints *
 
 @app.get('/api/news', response_model=list[Article])
-def read_news():
-    return get_news()
+def get_news():
+    return news_api.get_news()
 
+@app.get('/tasks/today', response_model=list[Task])
+def get_tasks_today():
+    return superprod_api.get_tasks_today()
 
 # * Start the server *
 
